11e3861ba82542d0690708e2cc06c00f892feee9
pandineer <pandineer@gmail.com>
2019-05-28 13:08:29 +0900

    done up to 3-3-1

---
+++ chapter3/taskleaf/app/controllers/tasks_controller.rb
@@ -0,0 +1,27 @@
+# coding: utf-8
+class TasksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
+end
