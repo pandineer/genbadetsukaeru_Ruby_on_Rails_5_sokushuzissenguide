2dfd42a0a9a19b1daa63765a4eb6ecdc5ef413a9
pandineer <pandineer@gmail.com>
2019-06-09 11:15:38 +0900

    done up to Chapter 7-5

--- chapter3/taskleaf/app/controllers/tasks_controller.rb
+++ chapter3/taskleaf/app/controllers/tasks_controller.rb
@@ -51,7 +51,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name, :description)
+    params.require(:task).permit(:name, :description, :image)
   end
 
   def set_task
